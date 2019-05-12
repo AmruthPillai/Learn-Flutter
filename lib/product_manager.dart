82feaa9cfec038a0e892d0e27478df398b73edea
Amruth Pillai <im.amruth@gmail.com>
2019-05-12 14:42:21 +0530

    adding print lines to understand lifecycle hooks

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -5,10 +5,15 @@ import 'package:learn_flutter/products.dart';
 class ProductManager extends StatefulWidget {
   final String startingProduct;
 
-  ProductManager(this.startingProduct);
+  ProductManager(this.startingProduct) {
+    print('[ProductManager Widget] Constructor');
+  }
 
   @override
-  _ProductManagerState createState() => _ProductManagerState();
+  _ProductManagerState createState() {
+    print('[ProductManager Widget] createState()');
+    return _ProductManagerState();
+  }
 }
 
 class _ProductManagerState extends State<ProductManager> {
@@ -17,11 +22,19 @@ class _ProductManagerState extends State<ProductManager> {
   @override
   void initState() {
     super.initState();
+    print('[ProductManager State] initState()');
     _products.add(widget.startingProduct);
   }
 
   @override
+  void didUpdateWidget(ProductManager oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    print('[ProductManager State] didUpdateWidget()');
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print('[ProductManager State] build()');
     return Column(children: <Widget>[
       Container(
         margin: EdgeInsets.all(10.0),
